2b4ce1e706368acea734b91b82fd91f4f99aaa29
Earl <earlmunyua96@gmail.com>
2023-11-10 23:53:45 +0300

    state and arrays test

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -6,26 +6,42 @@ function SpicyFoodList() {
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    const newArray= [...foods, newFood]
-    setFoods(newArray)
+    const newArray = [...foods, newFood];
+    setFoods(newArray);
     console.log(newFood);
   }
 
-  
   const foodList = foods.map((food) => (
-    <li key={food.id} onClick={() => handleClick (food.id)}>
+    <li key={food.id} onClick={() => handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
-  
-  function handleClick(id) {
-    const newFoodArray = foods.filter((food) => food.id !== id);
-  setFoods(newFoodArray);
+
+  // function handleLiClick(id) {
+  //   const newFoodArray = foods.filter((food) => food.id !== id);
+  //   setFoods(newFoodArray);
+  // }
+// this is for updating
+  function handleLiClick(id) {
+    const newFoodsArray = foods.map((food) => {
+      if (food.id === id) {
+        return {
+          ...food,
+          
+          heatLevel: food.heatLevel + 1,
+        };
+      } else {
+        return food;
+      }
+    });
+    setFoods(newFoodsArray);
   }
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
-      <ul>{foodList}</ul>
+      <ul>
+        {foodList}
+      </ul>
     </div>
   );
 }
